e43dd444f7a32f0e7975377ea8105c60f10c28a4
linyao <13340262648@163.com>
2022-03-29 22:00:35 +0800

    本地Vue项目请求本地Node.js服务器的配置方法

---
+++ vuedemo/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>接口返回的姓名是： {{ name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      name: '111'
+    }
+  },
+  mounted() {
+    this.axios.get('api/person').then(res => {
+      this.name = res.data.name
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vuedemo/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+        '/api': {
+            target: 'http://localhost:3000',   //node.js服务器运行的地址
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': 'http://localhost:3000'  //路径重写
+            }
+        },
+    }
+}
+})
